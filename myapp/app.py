bf175dc1de9645d733da9eb16e5efc607a2aaf1e
Hugo Duchene <duchenehugo10@gmail.com>
2020-06-11 15:59:02 +0100

    setup de l'app et l'insert user

--- myapp/app.py
+++ myapp/app.py
@@ -15,9 +15,70 @@ class Application:
             list_product = self.ApiManager.get_data(i)
             self.DatabaseManager.put_products(list_product)
 
-
         var_bool = True
         while var_bool:
-            User_enter_1 = int(input("1. Quel aliment souhaitez-vous remplacer ?, 2. Mes aliments substitués"))
+            User_enter_1 = int(input("1. Quel aliment souhaitez-vous remplacer ?, 2. Mes aliments substitués > "))
             if User_enter_1 == 1:
-                self.DatabaseManager.get_products("product_name", "category", "pizzas")
+                for nbs,cat in enumerate(self.list_categories):
+                    print(str(nbs) + " :"+ cat)
+
+                User_enter_2 = int(input("Choisissez votre catégorie de produits > "))
+
+                for nbs,cat in enumerate(self.list_categories):
+                    if User_enter_2 == nbs:
+                        list_food = self.DatabaseManager.get_products("product_name", "category", cat)
+                        for nubs,food in enumerate(list_food):
+                            (nutrient,) = food
+                            print(str(nubs)+ "." + nutrient)
+                            category = cat
+
+                User_enter_3 = int(input("Choisissez Votre aliment > "))
+
+                list_better_products = self.DatabaseManager.get_better_products(category)
+                for numbs, better_products in enumerate(list_better_products):
+                    (id, nutrient_grade, name, link, description, stores) = better_products
+                    print("-----------------------------Substitute-------------------------------------\n")
+
+                    print(str(numbs)+ " :" + name + " , " + description + " , " + stores + " , " + link + "\n")
+                    User_enter_3 = input("Appuyez sur une touche pour voir un autre résultat " + str(numbs) + "/2 OU appuyez sur 's' pour enregistrer le résultat")
+
+                    if User_enter_3 == "s":
+                        name_user = str(input("Dit moi ton nom : "))
+                        id = str(id)
+                        list_user = [(name_user, id)]
+                        self.DatabaseManager.record_user(list_user)
+                        var_bool = False
+
+
+    """def get_products_category(self):
+        for nbs,cat in enumerate(self.list_categories):
+            print(str(nbs) + " :"+ cat)
+
+    def choose_category(self, User_enter_2):
+        for nbs,cat in enumerate(self.list_categories):
+            if User_enter_2 == nbs:
+                list_food = self.DatabaseManager.get_products("product_name", "category", cat)
+                for nubs,food in enumerate(list_food):
+                    (nutrient,) = food
+                    print(str(nubs)+ "." + nutrient)
+                    category = cat
+
+        return category
+
+    def display_better_products(self, category):
+        list_better_products = self.DatabaseManager.get_better_products(category)
+        for numbs, better_products in enumerate(list_better_products):
+            (id, nutrient_grade, name, link, description, stores) = better_products
+            print("-----------------------------Substitute-------------------------------------\n")
+
+            print(str(numbs)+ " :" + name + " , " + description + " , " + stores + " , " + link + "\n")
+        return numbs
+
+    def option_record_user(self, User_enter_3, id):
+        if User_enter_3 == "s":
+            name_user = str(input("Dit moi ton nom : "))
+            list_user = [(name_user, id)]
+            self.DatabaseManager.record_user(list_user)
+            var_bool = False
+
+            return var_bool"""
--- myapp/models/database_manager.py
+++ myapp/models/database_manager.py
@@ -4,9 +4,9 @@ class DatabaseManager:
     """docstring forDatabaseManager."""
 
     def __init__(self):
-
         self.mydb = False
         self.mycursor = False
+        self.connect_database()
 
     def connect_database(self):
         self.mydb = mysql.connector.connect(
@@ -19,15 +19,24 @@ class DatabaseManager:
         self.mycursor = self.mydb.cursor()
 
     def get_products(self, column, place_table, result_table):
-        self.connect_database()
-        request_sql = "SELECT " + column +" FROM products WHERE "+ place_table +" = " + result_table
-        self.mycursor.execute(request_sql)
 
+        request_sql = "SELECT " + column +" FROM products WHERE "+ place_table +" = '" + result_table + "'"
+        self.mycursor.execute(request_sql)
         myresult = self.mycursor.fetchall()
         return myresult
 
     def put_products(self, list_product):
-        self.connect_database()
         request_sql = "INSERT IGNORE INTO products (category, product_name, ingredients_text, nutrition_grade, stores, brands, link) VALUES (%s, %s, %s, %s, %s, %s, %s)"
         self.mycursor.executemany(request_sql, list_product)
+        self.mydb.commit()
+
+    def get_better_products(self, category):
+        request_sql = "SELECT id, nutrition_grade, product_name, link, ingredients_text, stores FROM products WHERE category = '"+category+"' ORDER BY nutrition_grade LIMIT 0,3"
+        self.mycursor.execute(request_sql)
+        list_result = self.mycursor.fetchall()
+        return list_result
+
+    def record_user(self, list_user):
+        request_sql = "INSERT IGNORE INTO user_record (name, product_id) VALUES (%s , %s)"
+        self.mycursor.executemany(request_sql, list_user)
         self.mydb.commit()
