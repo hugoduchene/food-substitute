3e5a6349c58defe3b7b878e73b345090291dce49
Hugo Duchene <duchenehugo10@gmail.com>
2020-06-12 15:18:57 +0100

    finition des manage des inputs

--- myapp/app.py
+++ myapp/app.py
@@ -8,16 +8,27 @@ class Application:
         self.DatabaseManager = DatabaseManager()
         self.ApiManager = ApiManager()
         self.list_categories = ["pizzas", "spread", "snacks", "cheeses", "desserts"]
+        self.id_user = -1
 
     def main(self):
-        User_enter_1 = int(input("1. Quel aliment souhaitez-vous remplacer ?, 2. Mes aliments substitués >> "))
-        self.manage_first_entry(User_enter_1)
+        var = True
 
-        User_enter_2 = int(input("Sélectionnez votre catégorie >> "))
-        category = self.manage_second_entry(User_enter_2)
+        Connect_enter = int(input("1. Souhaitez-vous vous inscrire ? , 2. Souhaitez-vous vous connecter ? "))
+        var = self.registration(Connect_enter)
+        var = self.log(Connect_enter)
 
-        User_enter_3 = int(input("Sélectionnez votre produit >> "))
-        self.manage_third_entry(User_enter_3, category)
+        if var == True:
+            User_enter_1 = int(input("1. Quel aliment souhaitez-vous remplacer ?, 2. Mes aliments substitués >> "))
+            self.display_category(User_enter_1)
+            var = self.manage_products_user(User_enter_1)
+
+            if var == True:
+
+                User_enter_2 = int(input("Sélectionnez votre catégorie >> "))
+                category = self.manage_second_entry(User_enter_2)
+
+                User_enter_3 = int(input("Sélectionnez votre produit >> "))
+                self.manage_third_entry(User_enter_3, category)
 
     def insert_products(self):
         list_categories = self.list_categories
@@ -25,11 +36,28 @@ class Application:
             list_product = self.ApiManager.get_data(category)
             self.DatabaseManager.put_products(list_product)
 
-    def manage_first_entry(self, user_enter):
+    def display_category(self, user_enter):
         if user_enter == 1:
             list_categories = self.list_categories
             for nbs,category in enumerate(list_categories):
                 print(str(nbs) + " : " + category)
+
+
+    def manage_products_user(self, user_enter):
+        var_bool_products_user = True
+        if user_enter == 2:
+            list_id_products_record = self.DatabaseManager.get_id_products_record(self.id_user)
+            for tuple_id_product_record in list_id_products_record:
+                (id_product_record,) = tuple_id_product_record
+                id_product_record = str(id_product_record)
+                [(name, stores, link)] = self.DatabaseManager.get_products_record(id_product_record)
+                print("--------------------Vos éléments enregistrés ------------------------")
+                print(name + " - " + stores + " - " + link + "\n")
+                var_bool_products_user = False
+        return var_bool_products_user
+
+
+
 
     def manage_second_entry(self, user_enter):
         list_categories = self.list_categories
@@ -49,20 +77,35 @@ class Application:
                 (id, nutrient_grade, name, link, description, stores) = substitute
                 print("--------------------------------------------------SUBSTITUTE-----------------------------------------------------\n")
                 print(name + " , " + description + " , " + stores + " , " + link + "\n")
-                record_user = str(input("Tapez sur enter pour voir un autre produit "+ str(numbs) + "/2 OU seléctionnez s si vous souhaitez enregistrer le produit >> \n"))
-                self.record_user_product(record_user, name, id)
+                record_user_enter = str(input("Tapez sur enter pour voir un autre produit "+ str(numbs) + "/2 OU seléctionnez s si vous souhaitez enregistrer le produit >> \n"))
+                self.record_user_product(record_user_enter, id)
 
-    def record_user_product(self, record_user, name_product, id):
-        if record_user == "s":
-            user_name_enter = str(input("Saisis ton nom >> "))
-            list_product_user = (name_product, id)
+    def record_user_product(self, record_user_enter, id):
+        if record_user_enter == "s":
+            list_product_user = (self.id_user, id)
             self.DatabaseManager.record_user(list_product_user)
 
-    def log_inscription(self, user_enter):
+    def registration(self, user_enter):
+        if user_enter == 1:
+            pseudo = str(input("Votre pseudo >> "))
+            password = str(input("Votre mot de passe >> "))
+            mail = str(input("Votre email >> "))
+            tuple_infos = (pseudo, password, mail)
+            self.DatabaseManager.register_user(tuple_infos)
+        return True
+
+
+    def log(self, user_enter):
+        var = False
         if user_enter == 2:
-            choose_enter = int(input("1. Souhaitez-vous vous inscrire ? , 2. Souhaitez-vous vous connecter ? "))
-            if choose_enter == 1:
-                #inscrire
-            if choose_enter == 2:
-                #connect
-                #montre aliment 
+            pseudo = str(input("Votre pseudo >> "))
+            password = str(input("Votre mot de passe >> "))
+            (var_bool_connect ,id) = self.DatabaseManager.connect_user(pseudo, password)
+            self.id_user = str(id)
+
+
+            if var_bool_connect == True:
+                var_bool = True
+            else:
+                var_bool = False
+        return var_bool
--- myapp/models/database_manager.py
+++ myapp/models/database_manager.py
@@ -25,6 +25,18 @@ class DatabaseManager:
         myresult = self.mycursor.fetchall()
         return myresult
 
+    def get_id_products_record(self, id_user):
+        request_sql = "SELECT id_product FROM product_id WHERE id_user = "+ id_user 
+        self.mycursor.execute(request_sql)
+        myresult = self.mycursor.fetchall()
+        return myresult
+
+    def get_products_record(self, id_product):
+        request_sql = "SELECT product_name, stores, link FROM products WHERE id = "+ id_product
+        self.mycursor.execute(request_sql)
+        myresult = self.mycursor.fetchall()
+        return myresult
+
     def put_products(self, list_product):
         request_sql = "INSERT IGNORE INTO products (category, product_name, ingredients_text, nutrition_grade, stores, brands, link) VALUES (%s, %s, %s, %s, %s, %s, %s)"
         self.mycursor.executemany(request_sql, list_product)
@@ -37,6 +49,24 @@ class DatabaseManager:
         return list_result
 
     def record_user(self, list_user):
-        request_sql = "INSERT INTO user_record (user_name, id_product) VALUES (%s , %s)"
+        request_sql = "INSERT INTO product_id (id_user, id_product) VALUES (%s , %s)"
         self.mycursor.execute(request_sql, list_user)
         self.mydb.commit()
+
+    def register_user(self, list_infos):
+        request_sql = "INSERT INTO user (pseudo, password, mail) VALUES (%s, %s, %s)"
+        self.mycursor.execute(request_sql, list_infos)
+        self.mydb.commit
+
+    def connect_user(self, pseudo, password):
+        request_sql = "SELECT id, pseudo, password FROM user WHERE password = '"+password+"' AND pseudo = '"+pseudo+"'"
+        self.mycursor.execute(request_sql)
+        tuple_connect = self.mycursor.fetchall()
+        id = tuple_connect[0][0]
+
+        if tuple_connect == [(id, pseudo, password)]:
+            var = True
+        elif tuple_connect == []:
+            var = False
+
+        return (var, id)
